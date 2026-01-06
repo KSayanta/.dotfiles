246dff6376bbeb778fdeb02f08f05cf40d1b229c
KSayanta <181443436+KSayanta@users.noreply.github.com>
2026-01-06 21:18:01 +0530

    Update

--- nvim/.config/nvim/lua/config/plugins/mini.lua
+++ nvim/.config/nvim/lua/config/plugins/mini.lua
@@ -2,7 +2,25 @@ return {
   'nvim-mini/mini.nvim',
   version = '*',
   config = function()
-    -- Tabline
+    -- NOTE: Statusline
+    require('mini.statusline').setup({ use_icons = vim.g.have_nerd_font })
+
+    -- NOTE: AI
+    require('mini.ai').setup({ n_lines = 500 })
+
+    -- NOTE: Align
+    require('mini.align').setup()
+
+    -- NOTE: Autopairs
+    require('mini.pairs').setup()
+
+    -- NOTE: Indentscope
+    require('mini.indentscope').setup()
+
+    -- NOTE: Notify
+    require('mini.notify').setup()
+
+    -- NOTE: Tabline
     local MiniTabline = require('mini.tabline')
     MiniTabline.setup({
       show_icons = vim.g.have_nerd_font,
@@ -12,19 +30,7 @@ return {
       end,
     })
 
-    -- Statusline
-    require('mini.statusline').setup({ use_icons = vim.g.have_nerd_font })
-
-    -- AI
-    require('mini.ai').setup({ n_lines = 500 })
-
-    -- Align
-    require('mini.align').setup()
-
-    -- Autopairs
-    require('mini.pairs').setup()
-
-    -- Surround
+    -- NOTE: Surround
     require('mini.surround').setup({
       n_lines = 500,
       mappings = {
@@ -39,18 +45,18 @@ return {
       },
     })
 
-    -- Todo Comment Coloring
+    -- NOTE: Todo Comment Coloring
     require('mini.hipatterns').setup({
       highlighters = {
         -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-        fixme = { pattern = '%f[%w]()FIXME:()%f[%W]', group = 'MiniHipatternsFixme' },
-        hack = { pattern = '%f[%w]()HACK:()%f[%W]', group = 'MiniHipatternsHack' },
-        todo = { pattern = '%f[%w]()TODO:()%f[%W]', group = 'MiniHipatternsTodo' },
-        note = { pattern = '%f[%w]()NOTE:()%f[%W]', group = 'MiniHipatternsNote' },
+        fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+        hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+        todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+        note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
       },
     })
 
-    -- Files
+    -- NOTE: Files
     require('mini.files').setup({
       windows = { preview = true, width_preview = 50 },
       options = { permanent_delete = false },
@@ -63,8 +69,6 @@ return {
       callback = function(args)
         local win_id = args.data.win_id
         local config = vim.api.nvim_win_get_config(win_id)
-
-        -- Customize window-local settings
         vim.wo[win_id].winblend = 0
         config.border, config.title_pos = 'rounded', 'center'
         vim.api.nvim_win_set_config(win_id, config)
