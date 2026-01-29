79adb3a05ed790cfcdbc2b81d6da64104e4f57d6
KSayanta <181443436+KSayanta@users.noreply.github.com>
2026-01-29 18:33:31 +0530

    uodate treesitter

--- nvim/.config/nvim/lua/config/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/config/plugins/treesitter.lua
@@ -5,6 +5,7 @@ local languages = {
   'css',
   'diff',
   'html',
+  'javascript',
   'json',
   'lua',
   'luadoc',
@@ -12,6 +13,7 @@ local languages = {
   'markdown_inline',
   'query',
   'svelte',
+  'typescript',
   'vim',
   'vimdoc',
   'zig',
@@ -22,27 +24,12 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
-    branch = 'main',
-    build = ':TSUpdate',
     dependencies = { 'windwp/nvim-ts-autotag' },
     config = function()
       require('nvim-treesitter').install(languages)
-
       vim.api.nvim_create_autocmd('FileType', {
-        group = vim.api.nvim_create_augroup('treesitter.setup', {}),
-        callback = function(args)
-          local buf = args.buf
-          local filetype = args.match
-          local language = vim.treesitter.language.get_lang(filetype) or filetype
-          if not vim.treesitter.language.add(language) then
-            return
-          end
-
-          vim.treesitter.start(buf, language)
-          vim.wo.foldmethod = 'expr'
-          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
+        pattern = languages,
+        callback = function() vim.treesitter.start() end,
       })
     end,
   },
