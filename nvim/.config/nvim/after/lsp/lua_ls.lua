7fbe5b5832fef546f555deda92471940c3a3bc40
KSayanta <181443436+KSayanta@users.noreply.github.com>
2026-01-29 18:43:14 +0530

    update lsp config

--- nvim/.config/nvim/after/lsp/lua_ls.lua
+++ nvim/.config/nvim/after/lsp/lua_ls.lua
@@ -2,6 +2,23 @@ return {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
   single_file_support = true,
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT',
+        path = { 'lua/?.lua', 'lua/?/init.lua' },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+    })
+  end,
   settings = {
     Lua = {
       completion = { callSnippet = 'Replace' },
--- nvim/.config/nvim/lua/config/plugins/lsp.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp.lua
@@ -1,22 +1,9 @@
-local ensure_enabled = {
-  'astro',
-  'css_variables',
-  'cssls',
-  'cssmodules_ls',
-  'emmet_language_server',
-  'html',
-  'lua_ls',
-  'mdx_analyzer',
-  'svelte',
-  'tailwindcss',
-  'ts_ls',
-  'zls',
-}
-
 return {
   'neovim/nvim-lspconfig',
-  event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
+    { 'mason-org/mason.nvim', opts = {} },
+    'mason-org/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
     'saghen/blink.cmp',
   },
 
@@ -35,9 +22,42 @@ return {
       end,
     })
 
+    local servers = {
+      'astro',
+      'css_variables',
+      'cssls',
+      'cssmodules_ls',
+      'emmet_language_server',
+      'html',
+      'lua_ls',
+      'mdx_analyzer',
+      'svelte',
+      'tailwindcss',
+      'ts_ls',
+      'zls',
+    }
+
+    require('mason-lspconfig').setup({
+      automatic_enable = false,
+      automatic_installation = false,
+      ensure_installed = servers,
     })
 
-    -- Enable servers
-    vim.lsp.enable(ensure_enabled)
+    local tools = {
+      'lua-language-server', -- Lua language server
+      'stylua', -- Used to format Lua code
+      'prettier', -- Used to format HTML, CSS, Javascript
+      'prettierd', -- Used to format Javascript
+      'xmlformatter', -- Used to format xml / svg
+      -- 'eslint_d', -- Used to lint Javascript
+      -- 'markdownlint', -- Used to lint Markdown
+      -- 'stylelint', -- Used to lint CSS
+      -- 'jsonlint', -- Used to lint JSON
+      -- 'vale', -- Used to lint text
+    }
+
+    require('mason-tool-installer').setup({ ensure_installed = tools })
+
+    vim.lsp.enable(servers)
   end,
 }
