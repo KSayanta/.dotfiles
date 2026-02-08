f8a45b0a6c6daec2a56fddf2c6849794d26ad6a1
KSayanta <181443436+KSayanta@users.noreply.github.com>
2026-02-08 12:39:30 +0530

    add llama.vim

---
+++ nvim/.config/nvim/lua/config/plugins/llama.lua
@@ -0,0 +1,30 @@
+local host = '192.168.0.111'
+local port_fim = '8013'
+local port_inst = '8012'
+
+return {
+  'ggml-org/llama.vim',
+  init = function()
+    vim.g.llama_config = {
+      keymap_fim_trigger = '<C-space>',
+      keymap_fim_accept_full = '<C-f>',
+      keymap_fim_accept_line = '<C-L>',
+      keymap_fim_accept_word = '<C-]>',
+      keymap_inst_trigger = '<leader>li',
+      keymap_inst_rerun = '<leader>lr',
+      keymap_inst_continue = '<leader>lc',
+      keymap_inst_accept = '<leader>la',
+      keymap_inst_cancel = '<leader>lq',
+      keymap_debug_toggle = '<leader>ld',
+
+      enable_at_startup = false,
+      auto_fim = false,
+      endpoint_fim = 'http://' .. host .. ':' .. port_fim .. '/infill',
+      endpoint_inst = 'http://' .. host .. ':' .. port_inst .. '/v1/chat/completions',
+      -- stop_strings = { '\n' },
+
+      -- For Reference :help llama_config
+    }
+  end,
+  config = function() vim.keymap.set({ 'n' }, '<leader>lt', ':LlamaToggle<CR>', { desc = '[L]lama [T]oggle', silent = true }) end,
+}
