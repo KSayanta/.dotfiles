7b66e0c9054bb58ac569039bf480955a9a5e8ce7
KSayanta <181443436+KSayanta@users.noreply.github.com>
2026-06-19 17:16:01 +0530

    nvim update telescope

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -29,18 +29,19 @@ return {
     local map = vim.keymap.set
     local builtin = require('telescope.builtin')
 
+    map({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    map('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
+    map('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch with [G]rep' })
     map('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     map('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    map('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    map({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    map('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    map('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
     map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+    map('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    map('n', '<leader>st', builtin.colorscheme, { desc = '[S]earch [C]olorscheme' })
     map('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     map('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    map('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
-    map('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
 
     -- Search in Neovim configuration files
     map('n', '<leader>sn', function() builtin.find_files({ cwd = vim.fn.stdpath('config') }) end, { desc = '[S]earch [N]eovim files' })
